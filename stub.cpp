2c867d375d025eb3bc997ba98bbb895241332d7a
GauChoob <starlight_tonight@hotmail.com>
2025-12-09 13:43:43 -0500

    feat: sdl surface

--- stub.cpp
+++ stub.cpp
@@ -1,48 +1,21 @@
 #include "ALL.H"
 
-int main() { return 0; }
-
-int Locky0_alul = 0;
-
-unsigned char** lockbackbuffer(int unused) { return nullptr; }
-
-void unlockbackbuffer() {}
-
-unsigned char** lockfrontbuffer() { return nullptr; }
-
-void unlockfrontbuffer() {}
-
-ddpal::ddpal(unsigned char* tomb) {}
-ddpal::~ddpal() {}
-void ddpal::set() {}
-
-void uzenet(char* text1, char* text2, char* text3) { printf("%s %s %s\n", text1, text2, text3); }
-
-void hiba(char* text1, char* text2, char* text3) { printf("%s %s %s\n", text1, text2, text3); }
-
-int s_random(int maximum) { return 0; }
-
-void mv_check(int unused) {}
-
-void setmou(int x, int y) {}
-void getmou(int* x, int* y) {}
-
-double mv_stopperido(void) { return 0; }
-
-void mv_startstopper(void) {}
-
-int getbutbmou(void) { return 0; }
-
-int getbutjmou(void) { return 0; }
-
-int Billint = 0;
-
 int mk_kbhit(void) { return 0; }
 int mk_getstate(int unused) { return 0; }
 int mk_getextchar(void) { return 0; }
 
 void mk_emptychar(void) {}
 
+void mk_init() {}
+void mkw_getDIstate() {}
+void mkw_setkeydown() {}
+void initDI() {}
+void releaseDI() {}
+
 void initdsound(int secondaryeloir) {}
 int controlaltnyomva(void) { return 0; }
-void mv_exit(char* text) { exit(0); }
+
+void GetDXVersion(unsigned long* pdwDXVersion, unsigned long* pdwDXPlatform) {
+    *pdwDXVersion = 0x300;
+    *pdwDXPlatform = 0;
+}
